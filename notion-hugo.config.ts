d560209e122e565a5ce3340233393f7ad71c6f66
MetaIllusion <xkjing@outlook.com>
2025-03-29 18:42:49 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
     base_url: "https://notion-hugo.pages.dev",
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/metaillusion/Notion-Hugo-1c5149164bab806a87b4fa1a396b66f2?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
